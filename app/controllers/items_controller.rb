bca635b7308fe43d00532415d37466d7705b0918
Allison Bohannon <akboh.github@gmail.com>
2023-01-12 20:17:59 -0800

    commit tree 44f629de0074e55fd7232a91cd62c81b3f3c1791

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end 
     render json: items, include: :user
+  end
+
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+    else 
+      item = Item.find(params[:id])
+    end
+    render json: item, include: :user
+  end
+
+  def create 
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:id, :name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
